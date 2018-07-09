ac5da58ea7bbd395affc1923ce3e5efdcfb59d71
lianzeng <liang.1.zeng@nokia.com>
2018-07-09 16:47:55 +0800

    change vehicle value to string type

--- examples/client/object_vehicle.c
+++ examples/client/object_vehicle.c
@@ -26,43 +26,30 @@
  */
 #ifdef LWM2M_CLIENT_MODE
 
-#define RES_ID_M_STATE      0
-#define RES_ID_M_SPEED      1
-#define RES_ID_M_RPM        2
-#define RES_ID_M_TIMESTAMP  3
+#define RES_ID_M_STATE      0   //0-idle,1-driving,2-charging
+#define RES_ID_M_SPEED      1   //vehicle speed
+#define RES_ID_M_RPM        2   //engine speed
+#define RES_ID_M_TIMESTAMP  3   //sample time
 
-typedef  struct{
-    char rpm[10];//engine speed
-    float speed; //vehicle speed
-    unsigned long timestamp;
-    char   state[20];//0-idle,1-driving,2-charging
-}ObdData;
+#define RES_NUM_VEHICLE     4
+
+typedef InstanceData ObdData;
 
 static uint8_t prv_res2tlv(const ObdData* locDataP,
                            lwm2m_data_t* dataP)
 {
     //-------------------------------------------------------------------- JH --
-    uint8_t ret = COAP_205_CONTENT;
-    switch (dataP->id)     // location resourceId
+    for(int i = 0; i<locDataP->resNum; i++)
     {
-        case RES_ID_M_STATE:
-            lwm2m_data_encode_string(locDataP->state, dataP);
-            break;
-        case RES_ID_M_RPM:
-            lwm2m_data_encode_string(locDataP->rpm, dataP);
-            break;
-        case RES_ID_M_SPEED:
-            lwm2m_data_encode_float(locDataP->speed, dataP);
-            break;
-        case RES_ID_M_TIMESTAMP:
-            lwm2m_data_encode_int(locDataP->timestamp, dataP);
-            break;
-        default:
-            ret = COAP_404_NOT_FOUND;
-            break;
+        if(dataP->id == locDataP->resValues[i].resId)
+        {
+            lwm2m_data_encode_string(locDataP->resValues[i].value, dataP);
+            return COAP_205_CONTENT;
+        }
+
     }
 
-    return ret;
+    return  COAP_404_NOT_FOUND;
 }
 
 static uint8_t prv_vehicle_read(uint16_t instanceId,
@@ -80,21 +67,14 @@ static uint8_t prv_vehicle_read(uint16_t instanceId,
 
     if (*numResourceId == 0)     // full object, readable resources!
     {
-        uint16_t readResIds[] = {
-                RES_ID_M_STATE,
-                RES_ID_M_RPM,
-                RES_ID_M_SPEED,
-                RES_ID_M_TIMESTAMP
-        }; // readable resources!
-
-        *numResourceId  = sizeof(readResIds)/sizeof(readResIds[0]);
+        *numResourceId  = RES_NUM_VEHICLE;
         *tlvArrayP = lwm2m_data_new(*numResourceId);
         if (*tlvArrayP == NULL) return COAP_500_INTERNAL_SERVER_ERROR;
 
         // init readable resource id's
         for (i = 0 ; i < *numResourceId ; i++)
         {
-            (*tlvArrayP)[i].id = readResIds[i];
+            (*tlvArrayP)[i].id = locDataP->resValues[i].resId;
         }
     }
 
@@ -114,23 +94,24 @@ void display_vehicle_object(lwm2m_object_t * object)
     fprintf(stdout, "  /%u: vehicle object:\r\n", object->objID);
     if (NULL != data)
     {
-        fprintf(stdout, "  state: %s,  rpm: %s, speed: %.6f, timestamp: %lu \r\n",
-                data->state,data->rpm, data->speed, data->timestamp);
     }
 #endif
 }
 
 static void setResourceValue(const ResourceValue* rv, ObdData* obdData)
 {
-  switch(rv->resId)
+  for(int i = 0; i<obdData->resNum; i++)
   {
-      case RES_ID_M_STATE:
-          strncpy(obdData->state, rv->value, sizeof(obdData->state));
-          break;
-      default:
-          fprintf(stderr,"error,not support resId=%d \n", rv->resId);
-          break;
+      if(rv->resId == obdData->resValues[i].resId)
+      {
+          strncpy(obdData->resValues[i].value, rv->value, MAX_VALUE_LENGTH_PER_RESOURCE);
+          return;
+      }
+
   }
+
+  fprintf(stderr,"error,not support resId=%d \n", rv->resId);
+
 }
 
 void update_vehicle_measurement(const ObjectData* sensorData, lwm2m_context_t* context)
@@ -147,16 +128,24 @@ void update_vehicle_measurement(const ObjectData* sensorData, lwm2m_context_t* c
         {
             setResourceValue(&instData->resValues[i], obdData);
         }
-        //obdData->state = sensorData->state;
-        //obdData->rpm = sensorData->rpm;
-        //obdData->speed = sensorData->speed;
-        obdData->timestamp = lwm2m_gettime();
-
+        
     }
 
     lwm2m_uri_t urip = {LWM2M_URI_FLAG_OBJECT_ID, sensorData->objId, 0, 0};
     lwm2m_resource_value_changed(context, &urip);
 }
+
+static void initialResourceIds(ObdData* obdData)
+{
+    obdData->instId = 0;
+    obdData->resNum = RES_NUM_VEHICLE;
+    obdData->resValues[0].resId = RES_ID_M_STATE;
+    obdData->resValues[1].resId = RES_ID_M_SPEED;
+    obdData->resValues[2].resId = RES_ID_M_RPM;
+    obdData->resValues[3].resId = RES_ID_M_TIMESTAMP;
+}
+
+
 
 lwm2m_object_t * create_object_vehicle(void)
 {
@@ -194,6 +183,8 @@ lwm2m_object_t * create_object_vehicle(void)
         if (NULL != vehicleObj->userData)
         {
             memset(vehicleObj->userData, 0, sizeof(ObdData));
+            initialResourceIds(vehicleObj->userData);
+
         }
         else
         {
